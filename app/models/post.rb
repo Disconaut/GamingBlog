d6debb44aa8befeaf8248420e8275cb7cf62e486
Oles <omelanchuk98@gmail.com>
2020-05-30 19:32:49 +0300

    Added validation to post fields

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,8 @@ class Post < ApplicationRecord
   enum category: {news: 'news', reviews: 'reviews', others: 'others'}
   validates :category, inclusion: { in: categories.keys }
 
+  validates :title ,length: { minimum: 3, maximum: 255 }
+  validates :thumb_img, presence: true
+
   has_many :comments, as: :commentable
 end
